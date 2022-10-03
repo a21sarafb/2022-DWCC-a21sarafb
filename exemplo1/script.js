0038d3a0f9edcb64040cc116c8319f5c9c8bd929
a21sarafb <a21sarafb@iessanclemente.net>
2022-10-03 17:53:53 +0200

    Correcciones varias

--- exemplo1/script.js
+++ exemplo1/script.js
@@ -63,32 +63,20 @@ console.log(n);
 console.log("10" - "4" - "3" - 2 + "5");
 console.log("false" == false);*/
 
-/*1. Pide por teclado o día da semana (luns - domingo) e mostra unha mensaxe
-indicando se é laborable ou non.*/
-console.log(
+/*console.log(
 	"1. Pide por teclado o día da semana (luns - domingo) e mostra unha mensaxe indicando se é laborable ou non."
 );
 const dia = prompt("Ingrese un día da semana(luns - domingo): ");
 const diaMinusculas = dia.toLowerCase();
 switch (diaMinusculas) {
 	case "luns":
-		console.log(dia + " é laborable");
-		break;
 	case "martes":
-		console.log(dia + " é laborable");
-		break;
 	case "mercores":
-		console.log(dia + " é laborable");
-		break;
 	case "xoves":
-		console.log(dia + " é laborable");
-		break;
 	case "venres":
 		console.log(dia + " é laborable");
 		break;
 	case "sabado":
-		console.log(dia + " NON é laborable");
-		break;
 	case "domingo":
 		console.log(dia + " NON é laborable");
 		break;
@@ -97,33 +85,32 @@ switch (diaMinusculas) {
 			"Os valores válidos son: luns, martes, mercores, xoves, venres, sabado ou domingo"
 		);
 		break;
-}
+}*/
 
-/*2. Pide 3 números por teclado e indica cal é o maior ou se son iguais.*/
-console.log(
+/*console.log(
 	"2. Pide 3 números por teclado e indica cal é o maior ou se son iguais."
 );
-const num1 = prompt("Ingrese o primeiro número: ");
-const num2 = prompt("Ingrese o primeiro número: ");
-const num3 = prompt("Ingrese o primeiro número: ");
+const num1 = Number(prompt("Ingrese o primeiro número: "));
+const num2 = Number(prompt("Ingrese o primeiro número: "));
+const num3 = Number(prompt("Ingrese o primeiro número: "));
 if (num1 === num2 && num2 === num3) {
 	console.log(num1 + ", " + num2 + ", " + num3 + " son iguais");
-} else if (num1 > num2 && num2 > num3) {
+} else if (num1 > num2 && num1 > num3) {
 	console.log(num1 + " é o maior");
-} else if (num2 > num1 && num1 > num3) {
+} else if (num2 > num1 && num2 > num3) {
 	console.log(num2 + " é o maior");
-} else {
+} else if (num3 > num1 && num3 > num2) {
 	console.log(num3 + " é o maior");
-}
-/*3. Escribe os números pares do 0 ao 30.*/
-console.log("3. Escribe os números pares do 0 ao 30.");
+}*/
+
+/*console.log("3. Escribe os números pares do 0 ao 30.");
 let contador = 0;
 while (contador <= 30) {
 	console.log(contador);
 	contador += 2;
-}
-/*4. Escribe as potencias de 2, dende 2⁰ ata 2²⁰. Para cada potencia debe saír un texto similar a “2 elevado a 0 = 1”.*/
-console.log(
+}*/
+
+/*console.log(
 	"4. Escribe as potencias de 2, dende 2⁰ ata 2²⁰. Para cada potencia debe saír un texto similar a “2 elevado a 0 = 1”."
 );
 let contador2 = 0;
@@ -132,12 +119,8 @@ while (contador2 <= 20) {
 	console.log("2 elevado a " + contador2 + " = " + potencia);
 	contador2++;
 	potencia = 0;
-}
-/*5. Crea unha variable e asígnalle un número aleatorio entre 0 - 100. Elabora un
-programa para que o usuario adiviñe o número aleatorio. É dicir, debe pedirse un
-número por teclado ao usuario e informar se o número introducido é maior ou menor
-que o número a adiviñar. Volverase a pedir outro número ata que sexa adiviñado o
-secreto.*/
+}*/
+
 console.log(
 	"5. Crea unha variable e asígnalle un número aleatorio entre 0 - 100. Elabora un programa para que o usuario adiviñe o número aleatorio. É dicir, debe pedirse un número por teclado ao usuario e informar se o número introducido é maior ou menor que o número a adiviñar. Volverase a pedir outro número ata que sexa adiviñado o secreto."
 );
@@ -145,43 +128,47 @@ let aleatorio = random(0, 100);
 function random(min, max) {
 	return Math.floor(Math.random() * (max - min + 1) + min);
 }
-let adivina = prompt("Ingrese un número do 0 ao 100: " + aleatorio);
+let adivina = Number(prompt("Ingrese un número do 0 ao 100: " + aleatorio));
+while (isNaN(adivina)) {
+	adivina = Number(
+		prompt("DEBE SER UN NÚMERO. Ingrese outro número do 0 ao 100: ")
+	);
+}
 while (aleatorio != adivina) {
 	if (aleatorio > adivina) {
 		console.log(
 			"O número que introduciches é menor ao que debes acertar, volve a intentalo"
 		);
+		adivina = Number(prompt("Ingrese outro número do 0 ao 100: "));
 	} else if (aleatorio < adivina) {
 		console.log(
 			"O número que introduciches é maior ao que debes acertar, volve a intentalo"
 		);
+		adivina = Number(prompt("Ingrese outro número do 0 ao 100: "));
 	} else {
-		console.log("ACERTOU!!");
+		while (isNaN(adivina)) {
+			adivina = Number(
+				prompt("DEBE SER UN NÚMERO. Ingrese outro número do 0 ao 100: ")
+			);
+		}
 	}
-	adivina = prompt("Ingrese outro número do 0 ao 100: ");
 }
-/*6. Pide un número por teclado ao usuario. Calcula o factorial dese número e mostra a
-resultado por consola. (5! = 5*4*3*2*1).*/
-console.log(
+console.log("ACERTOU!");
+
+/*console.log(
 	"6. Pide un número por teclado ao usuario. Calcula o factorial dese número e mostra a resultado por consola. (5! = 5*4*3*2*1)."
 );
-let num = prompt("Ingrese un número para que calcule su factorial");
+let num = Number(prompt("Ingrese un número para que calcule su factorial"));
 let factorial = 1;
 for (i = 1; i <= num; i++) {
 	factorial = i * factorial;
 }
-console.log("O factorial de " + num + " é: " + factorial);
+console.log("O factorial de " + num + " é: " + factorial);*/
 
-/*7. Pide un número por teclado ao usuario. Despois crea unha variable de tipo array e
-asígnalle a lista de números enteiros dende o 0 ao número tecleado polo usuario.
-Ademais, fai a suma de todos os valores contidos no array e mostra por consola o
-resultado da suma.
-Utiliza un bucle for para recorrer o array. Proba a facelo das 3 formas posibles: for
-con contador, for…in e for…of.*/
-console.log(
+/*console.log(
 	"7. Pide un número por teclado ao usuario. Despois crea unha variable de tipo array e asígnalle a lista de números enteiros dende o 0 ao número tecleado polo usuario. Ademais, fai a suma de todos os valores contidos no array e mostra por consola o resultado da suma. Utiliza un bucle for para recorrer o array. Proba a facelo das 3 formas posibles: for con contador, for…in e for…of."
 );
-let arrayUsuario = prompt("Ingrese un número ");
+let arrayUsuario = Number(prompt("Ingrese un número "));
 const arr = [];
 let suma = 0;
 for (x = 0; x <= arrayUsuario; x++) {
@@ -200,38 +187,62 @@ for (let y in arr) {
 console.log("Array recorrido con for con contador: ");
 for (z = 0; z < arr.length; z++) {
 	console.log(z);
-}
+}*/
 
-/*8. Cálculo do IMC (índice de masa corporal):
-a. Almacena en variables o peso e altura de dúas persoas.
-b. Calcula o IMC das dúas persoas.
-c. Indica que persoa ten o maior IMC cunha cadea similar a: 'O IMC (25.3) da
-primeira persoa é maior que o da segunda persoa (22.5)!'*/
-console.log(
+/*console.log(
 	"8. Cálculo do IMC (índice de masa corporal): a. Almacena en variables o peso e altura de dúas persoas. b. Calcula o IMC das dúas persoas. c. Indica que persoa ten o maior IMC cunha cadea similar a: 'O IMC (25.3) da primeira persoa é maior que o da segunda persoa (22.5)!'"
 );
-const peso1 = prompt("Ingrese o peso da 1º persoa (en kilogramos)");
-const altura1 = prompt("Ingrese a altura da 1º persoa (en metros)");
-const peso2 = prompt("Ingrese o peso da 2º persoa (en kilogramos)");
-const altura2 = prompt("Ingrese a altura da 2º persoa (en metros)");
+const peso1 = Number(prompt("Ingrese o peso da 1º persoa (en kilogramos)"));
+const altura1 = Number(prompt("Ingrese a altura da 1º persoa (en metros)"));
+const peso2 = Number(prompt("Ingrese o peso da 2º persoa (en kilogramos)"));
+const altura2 = Number(prompt("Ingrese a altura da 2º persoa (en metros)"));
 const imc1 = peso1 / (altura1 * altura1);
 const imc2 = peso2 / (altura2 * altura2);
 if (imc1 > imc2) {
 	console.log(
 		"O IMC (" +
-			Math.round(imc1) +
+			imc1.toFixed(2) +
 			") da primeira persoa é maior que o da segunda persoa (" +
-			Math.round(imc2) +
+			imc2.toFixed(2) +
 			")!"
 	);
 } else if (imc2 > imc1) {
 	console.log(
 		"O IMC (" +
-			Math.round(imc2) +
+			imc2.toFixed(2) +
 			") da segunda persoa é maior que o da primeira persoa (" +
-			Math.round(imc1) +
+			imc1.toFixed(2) +
 			")!"
 	);
 } else {
-	console.log("Os dous IMC son iguais (" + Math.round(imc1) + ")");
-}
+	console.log("Os dous IMC son iguais (" + imc1.toFixed(2) + ")");
+}*/
+
+/*segunda ronda de exercicios*/
+console.log(
+	"1. Crea unha función frecha que devolva o cubo dun número pasado como parámetro"
+);
+
+console.log(
+	"2. Crea unha función á que se lle pase un array e devolva como resultado un array cos elementos impares do array de entrada. Exemplo: arrayEntrada = [10, 2, 3, 5, 7, 8, 23, 50] arraySaida = [3, 5, 7, 23]"
+);
+
+console.log(
+	"3. Crea unha función que sume todos os valores pasados como parámetros, sendo estes un número indeterminado."
+);
+
+console.log(
+	"4. Crea unha función á que se lle pasen varios números como parámetros (un número indeterminado de parámetros). Debe devolver a media dos números pasados. Proba a realizala con diferentes bucles for (con contador, for…in e for…of)."
+);
+
+console.log(
+	"5. Fai unha función á que se lle pase un DNI (ex: 12345678w ou 87654321T) e devolva se é correcto ou non"
+);
+
+console.log(
+	"6. Crea unha función que reciba un array bidimensional de lonxitude variable que se corresponda cun escenario do xogo de Buscaminas. Este array almacenará un -1 nas posicións onde hai minas e un 0 en caso contrario. A función debe devolver un array bidimensional onde cada posición que non teña mina, debe ter a información do número de minas adxacentes (diagonal, horizontal e vertical). Exemplo: arrayEntrada = [[0, 0, -1, 0], [0, -1, -1, 0]]; arraySaida = [[1, 3, -1, 2], [1, -1, -1, 2]];"
+);
+
+console.log(
+	"7. Crea unha función JavaScript que comprobe se é poxible axendar unha reunión dentro do horario laboral."
+);
