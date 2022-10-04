eae9a7a992e36685f091e1cb2bb4faece59d5701
a21sarafb <a21sarafb@iessanclemente.net>
2022-10-04 09:53:45 +0200

    Alguns cambios

--- exemplo1/script.js
+++ exemplo1/script.js
@@ -1,3 +1,4 @@
+"use strict";
 console.log("Mensaxe por consola");
 
 /*window.confirm("Mensaxe");
@@ -63,7 +64,7 @@ console.log(n);
 console.log("10" - "4" - "3" - 2 + "5");
 console.log("false" == false);*/
 
-/*console.log(
+console.log(
 	"1. Pide por teclado o día da semana (luns - domingo) e mostra unha mensaxe indicando se é laborable ou non."
 );
 const dia = prompt("Ingrese un día da semana(luns - domingo): ");
@@ -85,9 +86,9 @@ switch (diaMinusculas) {
 			"Os valores válidos son: luns, martes, mercores, xoves, venres, sabado ou domingo"
 		);
 		break;
-}*/
+}
 
-/*console.log(
+console.log(
 	"2. Pide 3 números por teclado e indica cal é o maior ou se son iguais."
 );
 const num1 = Number(prompt("Ingrese o primeiro número: "));
@@ -101,16 +102,15 @@ if (num1 === num2 && num2 === num3) {
 	console.log(num2 + " é o maior");
 } else if (num3 > num1 && num3 > num2) {
 	console.log(num3 + " é o maior");
-}*/
+}
 
-/*console.log("3. Escribe os números pares do 0 ao 30.");
+console.log("3. Escribe os números pares do 0 ao 30.");
 let contador = 0;
 while (contador <= 30) {
 	console.log(contador);
 	contador += 2;
-}*/
-
-/*console.log(
+}
+console.log(
 	"4. Escribe as potencias de 2, dende 2⁰ ata 2²⁰. Para cada potencia debe saír un texto similar a “2 elevado a 0 = 1”."
 );
 let contador2 = 0;
@@ -119,7 +119,7 @@ while (contador2 <= 20) {
 	console.log("2 elevado a " + contador2 + " = " + potencia);
 	contador2++;
 	potencia = 0;
-}*/
+}
 
 console.log(
 	"5. Crea unha variable e asígnalle un número aleatorio entre 0 - 100. Elabora un programa para que o usuario adiviñe o número aleatorio. É dicir, debe pedirse un número por teclado ao usuario e informar se o número introducido é maior ou menor que o número a adiviñar. Volverase a pedir outro número ata que sexa adiviñado o secreto."
@@ -155,23 +155,23 @@ while (aleatorio != adivina) {
 }
 console.log("ACERTOU!");
 
-/*console.log(
+console.log(
 	"6. Pide un número por teclado ao usuario. Calcula o factorial dese número e mostra a resultado por consola. (5! = 5*4*3*2*1)."
 );
 let num = Number(prompt("Ingrese un número para que calcule su factorial"));
 let factorial = 1;
-for (i = 1; i <= num; i++) {
+for (let i = 1; i <= num; i++) {
 	factorial = i * factorial;
 }
-console.log("O factorial de " + num + " é: " + factorial);*/
+console.log("O factorial de " + num + " é: " + factorial);
 
-/*console.log(
+console.log(
 	"7. Pide un número por teclado ao usuario. Despois crea unha variable de tipo array e asígnalle a lista de números enteiros dende o 0 ao número tecleado polo usuario. Ademais, fai a suma de todos os valores contidos no array e mostra por consola o resultado da suma. Utiliza un bucle for para recorrer o array. Proba a facelo das 3 formas posibles: for con contador, for…in e for…of."
 );
 let arrayUsuario = Number(prompt("Ingrese un número "));
 const arr = [];
 let suma = 0;
-for (x = 0; x <= arrayUsuario; x++) {
+for (let x = 0; x <= arrayUsuario; x++) {
 	arr.push(x);
 	suma = suma + x;
 }
@@ -185,11 +185,11 @@ for (let y in arr) {
 	console.log(y);
 }
 console.log("Array recorrido con for con contador: ");
-for (z = 0; z < arr.length; z++) {
+for (let z = 0; z < arr.length; z++) {
 	console.log(z);
-}*/
+}
 
-/*console.log(
+console.log(
 	"8. Cálculo do IMC (índice de masa corporal): a. Almacena en variables o peso e altura de dúas persoas. b. Calcula o IMC das dúas persoas. c. Indica que persoa ten o maior IMC cunha cadea similar a: 'O IMC (25.3) da primeira persoa é maior que o da segunda persoa (22.5)!'"
 );
 const peso1 = Number(prompt("Ingrese o peso da 1º persoa (en kilogramos)"));
@@ -216,7 +216,7 @@ if (imc1 > imc2) {
 	);
 } else {
 	console.log("Os dous IMC son iguais (" + imc1.toFixed(2) + ")");
-}*/
+}
 
 /*segunda ronda de exercicios*/
 console.log(
