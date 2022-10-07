c0b3839e71c3452093ad27c0439f1aa4075dc071
a21sarafb <a21sarafb@iessanclemente.net>
2022-10-07 10:25:50 +0200

    Algunas correcciones en ejercicios funciones

--- exemplo1/script.js
+++ exemplo1/script.js
@@ -225,6 +225,7 @@ if (imc1 > imc2) {
 const numero = Number(prompt("Ingrese o numero que desexa elevar ao cubo"));
 const cubo = (numero) => numero * numero * numero;
 console.log(cubo(numero));*/
+
 /*console.log(
 	"2. Crea unha función á que se lle pase un array e devolva como resultado un array cos elementos impares do array de entrada. Exemplo: arrayEntrada = [10, 2, 3, 5, 7, 8, 23, 50] arraySaida = [3, 5, 7, 23]"
 );
@@ -239,30 +240,37 @@ const arrayImpar = (arrayEntrada) => {
 	return arraySaida;
 };
 console.log(arrayImpar(arrayEntrada));*/
+
 /*console.log(
 	"3. Crea unha función que sume todos os valores pasados como parámetros, sendo estes un número indeterminado."
 );
-let bucle = true;
-const arraySuma = [];
-let suma = 0;
-while (bucle) {
-	const numPedido = Number(
-		prompt("Introduzca el número y escriba 0 cuando acabe")
-	);
-	if (numPedido != 0) {
-		arraySuma.push(numPedido);
-	} else {
-		bucle = false;
+const funcionSuma = (...theArgs) => {
+	let suma = 0;
+	for (const argumentos of theArgs) {
+		suma += argumentos;
 	}
-}
-for (let i = 0; i < arraySuma.length; i++) {
-	suma += arraySuma[i];
-}
+	return suma;
+};
+console.log(funcionSuma(1, 2, 3));
+console.log(funcionSuma(1, 2, 3, 4, 5));
+console.log(funcionSuma(1, 2, 3, 6));*/
 
-console.log(suma);*/
 /*console.log(
 	"4. Crea unha función á que se lle pasen varios números como parámetros (un número indeterminado de parámetros). Debe devolver a media dos números pasados. Proba a realizala con diferentes bucles for (con contador, for…in e for…of)."
-);*/
+);
+const funcionMedia = (...theArgs) => {
+	let media = 0;
+	let suma = 0;
+	for (const args of theArgs) {
+		media += args;
+		suma++;
+	}
+	return media / suma;
+};
+
+console.log(funcionMedia(2, 4, 6, 8));
+console.log(funcionMedia(4, 4));
+console.log(funcionMedia(4, 4, 6, 6, 8, 8));*/
 
 /*console.log(
 	"5. Fai unha función á que se lle pase un DNI (ex: 12345678w ou 87654321T) e devolva se é correcto ou non"
@@ -292,10 +300,98 @@ const checkDni = (dni) => {
 	}
 };
 console.log(checkDni(dni));*/
+
 /*console.log(
 	"6. Crea unha función que reciba un array bidimensional de lonxitude variable que se corresponda cun escenario do xogo de Buscaminas. Este array almacenará un -1 nas posicións onde hai minas e un 0 en caso contrario. A función debe devolver un array bidimensional onde cada posición que non teña mina, debe ter a información do número de minas adxacentes (diagonal, horizontal e vertical). Exemplo: arrayEntrada = [[0, 0, -1, 0], [0, -1, -1, 0]]; arraySaida = [[1, 3, -1, 2], [1, -1, -1, 2]];"
 );
+
+
 */
-/*console.log(
+console.log(
 	"7. Crea unha función JavaScript que comprobe se é poxible axendar unha reunión dentro do horario laboral."
-);*/
+);
+const inicioXornada = "07:30";
+const finalXornada = "17:45";
+const inicioXornadaSeparada = inicioXornada.split(":");
+const finalXornadaSeparada = finalXornada.split(":");
+const horaInicioXornada = parseInt(inicioXornadaSeparada[0]);
+const minsInicioXornada = parseInt(inicioXornadaSeparada[1]);
+const horaFinalXornada = parseInt(finalXornadaSeparada[0]);
+const minsFinalXornada = parseInt(finalXornadaSeparada[1]);
+let dentroHorario = true;
+const axendarReunion = (horaInicioReunion, duracionEnMinutos) => {
+	const limpio = horaInicioReunion.split(":");
+	const hora = parseInt(limpio[0]);
+	const mins = parseInt(limpio[1]);
+	let minsAcabaReunion = duracionEnMinutos + mins;
+	let minsDespuesReunion = 0;
+	let horaDespuesReunion = 0;
+	while (minsAcabaReunion >= 60) {
+		minsDespuesReunion = minsAcabaReunion - 60;
+		horaDespuesReunion = hora + 1;
+		minsAcabaReunion -= 60;
+	}
+	if (hora < horaInicioXornada) {
+		dentroHorario = false;
+	} else if (hora == horaInicioXornada && mins < minsInicioXornada) {
+		dentroHorario = false;
+	} else if (
+		hora > horaInicioXornada &&
+		mins > minsInicioXornada &&
+		horaDespuesReunion > horaFinalXornada
+	) {
+		dentroHorario = false;
+	} else if (
+		hora >= horaInicioXornada &&
+		mins >= minsInicioXornada &&
+		horaDespuesReunion <= horaFinalXornada &&
+		minsDespuesReunion > minsFinalXornada
+	) {
+		dentroHorario = false;
+	}
+	return dentroHorario;
+};
+
+console.assert(
+	axendarReunion("7:00", 15) == false,
+	'Fallo comprobando axendarReunión("7:00", 15) == false'
+);
+console.assert(
+	axendarReunion("7:15", 30) == false,
+	'Fallo comprobando axendarReunión("7:15", 30) == false'
+);
+console.assert(
+	axendarReunion("7:30", 30) == true,
+	'Fallo comprobando axendarReunión("7:30", 30) == false'
+);
+console.assert(
+	axendarReunion("11:30", 60) == true,
+	'Fallo comprobando axendarReunión("17:45", 60) == false'
+);
+console.assert(
+	axendarReunion("17:00", 45) == true,
+	'Fallo comprobando axendarReunión("11:30", 60) == false'
+);
+console.assert(
+	axendarReunion("17:30", 30) == false,
+	'Fallo comprobando axendarReunión("17:30", 30) == false'
+);
+
+/*console.log(
+	"8. Crea unha función chamada buscarPatron(texto, patron) que reciba como parámetros un texto e un patrón. A función debe devolver como resultado o número de veces que aparece o patrón no texto. Hai que implementar a función de forma manual e non utilizar as funcións proporcionadas pola linguaxe JavaScript. Non se deben distinguir maiúsculas de minúsculas. Un carácter pode formar parte de máis dun patrón encontrado. Exemplo: buscarPatron(“000111101000ABCHO”, “00”) debe devolver 4."
+);
+const buscarPatron = (texto, patron) => {
+	
+}
+buscarPatron(“000111101000ABCHO”, “00”);
+};*/
+/*console.log(
+	"9. Crea unha función que reciba como parámetro unha cantidade enteira e faga o desglose do número de billetes e moedas necesarios para obtela. Debe usarse o número mínimo de billetes e moedas."
+);
+const cambio = (dinero) => {
+}
+console.log(cambio(56));
+console.log(cambio(25));
+console.log(cambio(37));
+console.log(cambio(138));
+*/
