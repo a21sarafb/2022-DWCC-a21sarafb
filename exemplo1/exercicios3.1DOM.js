dadab057baa055752a330db92172df66c6da7f99
a21sarafb <a21sarafb@iessanclemente.net>
2022-11-13 13:55:28 +0100

    13-11-2022

---
+++ exemplo1/exercicios3.1DOM.js
@@ -0,0 +1,12 @@
+//o nodo <div>
+let primeiro = document.getElementsByTagName("div");
+console.log(primeiro[0]);
+console.log(primeiro[0].textContent);
+//o nodo <ul>
+let segundo = document.getElementsByTagName("ul");
+console.log(segundo[0]);
+console.log(segundo[0].textContent);
+//o segundo <li>
+let tercero = document.getElementsByTagName("li");
+console.log(tercero[1]);
+console.log(tercero[1].textContent);
---
+++ exemplo1/exercicios3.3DOM.js
@@ -0,0 +1,17 @@
+const proba = document.getElementById("primera");
+let primeiro = proba.firstElementChild;
+primeiro.style.backgroundColor = "red";
+
+const proba2 = document.getElementsByTagName("td");
+
+let segundo = proba2[6];
+segundo.style.backgroundColor = "red";
+
+let tercero = proba2[12];
+tercero.style.backgroundColor = "red";
+
+let cuarto = proba2[18];
+cuarto.style.backgroundColor = "red";
+
+let quinto = proba2[24];
+quinto.style.backgroundColor = "red";
