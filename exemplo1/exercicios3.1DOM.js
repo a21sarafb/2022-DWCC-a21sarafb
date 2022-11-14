31004fb04e6b7a2304186c9ea78b7bb81a5b0ca9
a21sarafb <a21sarafb@iessanclemente.net>
2022-11-14 17:26:50 +0100

    14-11-2022

--- exemplo1/exercicios3.1DOM.js
+++ exemplo1/exercicios3.1DOM.js
@@ -1,12 +1,29 @@
+"use strict";
 //o nodo <div>
 let primeiro = document.getElementsByTagName("div");
 console.log(primeiro[0]);
 console.log(primeiro[0].textContent);
+//OUTRA
+let primeiroDiv = document.body.firstElementChild;
+console.log(primeiroDiv);
+//OUTRA
+let primerDiv = document.body.children;
+console.log(primerDiv[0]);
 //o nodo <ul>
 let segundo = document.getElementsByTagName("ul");
 console.log(segundo[0]);
 console.log(segundo[0].textContent);
+//OUTRA
+let primerUl = document.body.children;
+console.log(primerUl[1]);
+//OUTRA
+let primeiroUl = document.body.firstElementChild.nextElementSibling;
+console.log(primeiroUl);
 //o segundo <li>
 let tercero = document.getElementsByTagName("li");
 console.log(tercero[1]);
 console.log(tercero[1].textContent);
+//OUTRA
+let segundoLi =
+	document.body.firstElementChild.nextElementSibling.lastElementChild;
+console.log(segundoLi);
--- exemplo1/exercicios3.3DOM.js
+++ exemplo1/exercicios3.3DOM.js
@@ -1,10 +1,14 @@
-const proba = document.getElementById("primera");
+/*const proba = document.getElementById("primera");
 let primeiro = proba.firstElementChild;
-primeiro.style.backgroundColor = "red";
+primeiro.style.backgroundColor = "red";*/
 
 const proba2 = document.getElementsByTagName("td");
-
-let segundo = proba2[6];
+for (let a = 0; a < proba2.length; a++) {
+	if (a == 0 || a == 6 || a == 12 || a == 18 || a == 24) {
+		proba2[a].style.backgroundColor = "red";
+	}
+}
+/*let segundo = proba2[6];
 segundo.style.backgroundColor = "red";
 
 let tercero = proba2[12];
@@ -14,4 +18,4 @@ let cuarto = proba2[18];
 cuarto.style.backgroundColor = "red";
 
 let quinto = proba2[24];
-quinto.style.backgroundColor = "red";
+quinto.style.backgroundColor = "red";*/
