b8ab4357ecbde184aa5c47a76f7f8c4ea87abe2d
a21sarafb <a21sarafb@iessanclemente.net>
2022-11-20 21:48:23 +0100

    20-11-2022

--- exemplo1/exercicios3.9DOM.js
+++ exemplo1/exercicios3.9DOM.js
@@ -1,1 +1,12 @@
 "use strict";
+let li = document.getElementsByTagName("li");
+console.log(li);
+for (const elim of li) {
+	elim.remove();
+}
+let x = document.querySelectorAll("li");
+let cantidad = x.length;
+if (cantidad > 0) {
+	x = x[cantidad - 1];
+	x.remove();
+}
