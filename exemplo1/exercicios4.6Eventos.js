c78b3f25d9f9e6c4eebcbb428c9b151b5219c030
a21sarafb <a21sarafb@iessanclemente.net>
2022-11-24 11:07:28 +0100

    24-11-2022

--- exemplo1/exercicios4.6Eventos.js
+++ exemplo1/exercicios4.6Eventos.js
@@ -1,1 +1,9 @@
 "use strict";
+let boton = document.getElementsByClassName("remove-button");
+console.log(boton);
+let texto = document.getElementsByClassName("pane");
+console.log(texto);
+function elim() {
+	texto.classList.add("hidden");
+}
+boton.addEventListener("click", elim);
