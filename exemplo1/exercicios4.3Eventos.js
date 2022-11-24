4540970ae32ff5ec01b8d0d6370743247710e06c
a21sarafb <a21sarafb@iessanclemente.net>
2022-11-24 10:41:19 +0100

    24-11-2022

--- exemplo1/exercicios4.3Eventos.js
+++ exemplo1/exercicios4.3Eventos.js
@@ -1,1 +1,11 @@
 "use strict";
+let boton = document.getElementById("engadir");
+function añadirTexto() {
+	let ul = document.querySelector(".lista");
+	let texto = document.getElementById("textoExercicio3");
+	let liLast = document.createElement("li");
+	let text = texto.value;
+	liLast.innerHTML = text;
+	ul.append(liLast);
+}
+boton.addEventListener("click", añadirTexto);
