08d7133e2993f754a07599bb10b22aa2d6cde70b
unknown <a21sarafb@iessanclemente.net>
2022-11-29 22:14:23 +0100

    29-11-2022

--- exemplo1/exercicios4.10Eventos.js
+++ exemplo1/exercicios4.10Eventos.js
@@ -1,1 +1,8 @@
 "use strict";
+let ul = document.getElementById("thumbs");
+ul.addEventListener("click", fotitos);
+function fotitos(e) {
+	e.preventDefault();
+	let pulsar = e.target.closest("a");
+	console.log(pulsar);
+}
--- exemplo1/exercicios4.11Eventos.js
+++ exemplo1/exercicios4.11Eventos.js
@@ -1,1 +1,17 @@
 "use strict";
+let lista = document.getElementById("ul");
+lista.addEventListener("click", seleccionar);
+function seleccionar(e) {
+	let pulsar = e.target.closest("li");
+	let li = document.querySelectorAll("li");
+	console.log(li);
+	li.forEach((esto) => {
+		if (esto.hasAttribute("class")) {
+			esto.removeAttribute("class");
+		}
+		console.log(esto);
+	});
+	console.log(pulsar);
+
+	pulsar.classList.add("selected");
+}
