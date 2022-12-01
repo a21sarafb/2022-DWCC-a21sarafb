bb3e0a52c25e74c42ae8e7d1ed99fa81c7a45b4a
unknown <a21sarafb@iessanclemente.net>
2022-12-01 19:57:27 +0100

    01-12-2022

--- exemplo1/exercicios5.1Formularios.js
+++ exemplo1/exercicios5.1Formularios.js
@@ -1,1 +1,11 @@
 "use strict";
+let formulario = document.getElementById("seleccion");
+formulario.addEventListener("input", seleccionar);
+let seleccion = document.getElementById("seleccion");
+function seleccionar() {
+	console.log("A opción escollida é: " + seleccion.value);
+}
+let opcion = document.createElement("option");
+opcion.innerHTML = 5;
+opcion.value = "5";
+seleccion.append(opcion);
--- exemplo1/exercicios5.2Formularios.js
+++ exemplo1/exercicios5.2Formularios.js
@@ -1,1 +1,22 @@
 "use strict";
+//validación con JavaScript
+let nums = document.getElementById("nums");
+nums.addEventListener("input", seleccionar);
+function seleccionar() {
+	let patron = nums.getAttribute("pattern");
+	if (patron.match()) {
+		error.html("<span>I am expecting 8 numbers or 3nums-2nums-3nums</span>");
+		return false;
+	}
+}
+//validacion con API
+/*let nums = document.getElementById("nums");
+nums.addEventListener("input", seleccionar);
+function seleccionar(e) {
+	if (nums.validity.patternMismatch) {
+		nums.setCustomValidity("I am expecting 8 numbers or 3nums-2nums-3nums");
+		nums.reportValidity();
+	} else {
+		nums.setCustomValidity("");
+	}
+}*/
