7ce8b717ddfe3645b5379a74baa36282a49e705c
a21sarafb <a21sarafb@iessanclemente.net>
2023-01-09 18:00:18 +0100

    09-01-23

--- exemplo1/2eval/exercicios1.1Ajax.js
+++ exemplo1/2eval/exercicios1.1Ajax.js
@@ -2,7 +2,7 @@
 /* Implementa unha función escribirNumeros(desde, ata) que xere un número cada
 segundo, comezando en “desde” e rematando en “ata”. Fai dúas variantes da
 función: unha usando setInterval e outra usando setTimeout. */
-function escribirNumeros(desde, ata) {
+/*function escribirNumeros(desde, ata) {
 	let index = 1000;
 	for (let i = desde; i <= ata; i++) {
 		setTimeout(() => {
@@ -11,7 +11,13 @@ function escribirNumeros(desde, ata) {
 		index += 1000;
 	}
 }
-console.log(escribirNumeros(5, 10));
+console.log(escribirNumeros(5, 10));*/
 
-const intervalID = setInterval(escribirNumeros2, 1000);
-function escribirNumeros2(desde2, ata2) {}
+const intervalID = setInterval(escribirNumeros2, 1000, 3, 8);
+function escribirNumeros2(desde2, ata2) {
+	for (let i = desde2; i <= ata2; i++) {
+		console.log(i);
+	}
+	clearTimeout(intervalID);
+}
+console.log(intervalID);
