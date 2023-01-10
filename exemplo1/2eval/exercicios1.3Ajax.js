b58614e2bf32f16f7f221abd4d967323f3f61a34
a21sarafb <a21sarafb@iessanclemente.net>
2023-01-10 20:31:45 +0100

    10-01-2023

--- exemplo1/2eval/exercicios1.3Ajax.js
+++ exemplo1/2eval/exercicios1.3Ajax.js
@@ -17,4 +17,45 @@ request.send();
 request.addEventListener("load", function () {
 	const gatitos = request.response;
 	console.log(gatitos);
+	/*console.log(
+		"Os nomes das gatas son " +
+			gatitos[0].name +
+			", " +
+			gatitos[1].name +
+			" e " +
+			gatitos[2].name
+	);*/
+	let arrayNomes = [];
+	let contadorGatos = 0;
+	let contadorFemias = 0;
+	let contadorMachos = 0;
+	for (let clave in gatitos) {
+		arrayNomes.push(gatitos[clave].name);
+		let kittens = gatitos[clave].kittens;
+		Object.entries(kittens).forEach(([key, value]) => {
+			if (value.gender === "m") {
+				contadorMachos++;
+			} else {
+				contadorFemias++;
+			}
+		});
+		contadorGatos += gatitos[clave].kittens.length;
+	}
+	let cadea1 = "Os nomes das gatas son ";
+	for (let i = 0; i < arrayNomes.length - 1; i++) {
+		cadea1 += arrayNomes[i];
+		if (i != arrayNomes.length - 2) cadea1 += ", ";
+	}
+	cadea1 += " e ";
+	cadea1 += arrayNomes[arrayNomes.length - 1];
+	console.log(cadea1);
+	console.log(
+		"Hai " +
+			contadorGatos +
+			" gatiños: " +
+			contadorFemias +
+			" femias e " +
+			contadorMachos +
+			" machos."
+	);
 });
