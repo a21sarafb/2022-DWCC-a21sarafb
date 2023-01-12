6bd284157d002fc5f021b1a08f1b6cbc7ec1c15a
a21sarafb <a21sarafb@iessanclemente.net>
2023-01-12 11:16:52 +0100

    12/01/23

--- exemplo1/2eval/exercicioProposto.js
+++ exemplo1/2eval/exercicioProposto.js
@@ -1,7 +1,7 @@
 const request = new XMLHttpRequest();
 request.open(
 	"GET",
-	"https://api.edamam.com/api/recipes/v2?type=public&app_id=3ec28d79&app_key=fcd09282a9404523092a4a1d33ad13f3&ingr=5-6&diet=balanced&health=alcohol-free&cuisineType=Asian&dishType=Starter&imageSize=SMALL&field=uri"
+	"https://api.edamam.com/api/recipes/v2?type=public&app_id=3ec28d79&app_key=fcd09282a9404523092a4a1d33ad13f3&ingr=5-6&diet=balanced&health=alcohol-free&cuisineType=Asian&dishType=Starter&imageSize=SMALL&field=image"
 
 	/*https://world.openfoodfacts.org/api/v0/product/737628064502.json*/
 	/*https://poetrydb.org/title/all/author,title,lines,linecount.json*/
