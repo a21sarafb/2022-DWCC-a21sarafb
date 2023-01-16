b8ee53c84eac39d28220f44559a66c97e0a17bda
a21sarafb <a21sarafb@iessanclemente.net>
2023-01-16 17:27:12 +0100

    16-01-2023

--- exemplo1/2eval/Ajax/exercicios1.9Ajax.js
+++ exemplo1/2eval/Ajax/exercicios1.9Ajax.js
@@ -1,7 +1,6 @@
 "use strict";
-/* 1. ¿Son iguais os seguintes fragmentos de código? ¿Que pasa se se produce un erro
-en f1?
+/* 1. ¿Son iguais os seguintes fragmentos de código? ¿Que pasa se se produce un erro en f1?
 promise.then(f1).catch(f2);
 promise.then(f1, f2); */
-promise.then(f1).catch(f2);
-promise.then(f1, f2);
+
+//non poderá captarse
