8b8deb80e0ab89ec16c39e0b69da7729b0c5fbd2
a21sarafb <a21sarafb@iessanclemente.net>
2023-01-18 22:57:23 +0100

    18-01-2023

--- exemplo1/2eval/Ajax/exercicios1.15Ajax.js
+++ exemplo1/2eval/Ajax/exercicios1.15Ajax.js
@@ -17,8 +17,13 @@ Na ligazón de consulta da habilidade aparece o seu nome en diferentes idiomas.
 Busca o nome en español e móstrao tamén na páxina web. */
 const log = document.querySelector(".event-log");
 document.querySelector("#xhr").addEventListener("click", () => {
+	//mostrar reload
+	let reload = document.querySelector("#reload");
+	reload.setAttribute("class", "raise");
+
+	//elaborar url co numero aleatorio
 	const comezoUrl = "https://pokeapi.co/api/v2/pokemon/";
-	const numId = Math.floor(Math.random() * (1000 - 1) + 1);
+	const numId = Math.floor(Math.random() * (905 - 1) + 1);
 	console.log(numId);
 	let numIdString = numId.toString();
 	const urlFinal = comezoUrl.concat(numIdString);
@@ -28,14 +33,119 @@ document.querySelector("#xhr").addEventListener("click", () => {
 			if (response.ok) return response.json();
 			return Promise.reject(response);
 		})
+		//obtención de datos
 		.then((data) => {
 			console.log(data);
-		})
+			//nome
+			let newParrafo = document.createElement("h1");
+			newParrafo.innerHTML = "Nome: " + data.name.toUpperCase();
+			let divNome = document.querySelector(".nome");
+			divNome.append(newParrafo);
 
+			//habilidades
+			let divHabilidades = document.querySelector(".habilidades");
+			divHabilidades.innerHTML = "Habilidades: ";
+			let arrayHabilidades = data.abilities;
+			let lista = document.createElement("ul");
+			arrayHabilidades.forEach(function (habilidad) {
+				console.log(habilidad);
+				let elementoLista = document.createElement("li");
+				let enlaceHabilidades = document.createElement("a");
+				enlaceHabilidades.setAttribute("target", "_blank");
+				enlaceHabilidades.setAttribute("href", habilidad.ability.url);
+				//obtendo a url para poñer a habilidade en español
+				fetch(habilidad.ability.url)
+					.then((response) => {
+						if (response.ok) return response.json();
+						return Promise.reject(response);
+					})
+					//obtención de datos de habilidad en español
+					.then((data) => {
+						console.log(data);
+						//nombre español
+						let arrayIdiomasHabilidad = data.names;
+						arrayIdiomasHabilidad.forEach(function (idioma) {
+							if (idioma.language.name === "es") {
+								enlaceHabilidades.innerHTML = idioma.name;
+							}
+						});
+					})
+					.catch(function (error) {
+						console.warn("Something went wrong.", error.message);
+					});
+
+				elementoLista.append(enlaceHabilidades);
+				lista.append(elementoLista);
+			});
+			divHabilidades.append(lista);
+
+			//imaxe
+			let bodyImaxe = document.querySelector(".imaxe");
+			let urlImaxe = data.sprites.other.home.front_default;
+			let imaxe = document.createElement("img");
+			imaxe.setAttribute("src", urlImaxe);
+			imaxe.setAttribute("width", 300);
+			imaxe.setAttribute("height", 300);
+			imaxe.setAttribute("alt", data.name);
+			bodyImaxe.append(imaxe);
+
+			//tipo
+			let divTipo = document.querySelector(".tipo");
+			divTipo.innerHTML = "Tipo: ";
+			let arrayTipo = data.types;
+			arrayTipo.forEach(function (tipos) {
+				let nombreTipo = tipos.type.name;
+				let urlTipo = tipos.type.url;
+				console.log(nombreTipo);
+				console.log(urlTipo);
+				fetch(urlTipo)
+					.then((response) => {
+						if (response.ok) return response.json();
+						return Promise.reject(response);
+					})
+					//obtención de datos de tipo en español
+					.then((data) => {
+						console.log("aqui");
+						console.log(data);
+						let arrayIdiomasTipo = data.names;
+						//nombre español + imagen
+
+						arrayIdiomasTipo.forEach(function (idioma) {
+							if (idioma.language.name === "es") {
+								let imagenTipo = document.createElement("img");
+								let urlImaxeTipoP = "img\\";
+								let tipoTexto = idioma.name.toLowerCase();
+								let urlImaxeTipoF = ".png";
+								console.log("ESTO");
+
+								let urlTipoFinal = urlImaxeTipoP.concat(
+									tipoTexto,
+									urlImaxeTipoF
+								);
+								console.log(urlTipoFinal);
+								imagenTipo.setAttribute("src", urlTipoFinal);
+								imagenTipo.setAttribute("width", 100);
+								imagenTipo.setAttribute("height", 30);
+								imagenTipo.setAttribute("alt", idioma.name);
+								divTipo.append(imagenTipo);
+							}
+						});
+					})
+					.catch(function (error) {
+						console.warn("Something went wrong.", error.message);
+					});
+			});
+
+			//ocultar boton de xenerar
+			let boton = document.querySelector("#xhr");
+			boton.setAttribute("class", "hidden");
+		})
+		//captura de erros
 		.catch(function (error) {
 			console.warn("Something went wrong.", error.message);
 		});
 });
+//evento al pulsar reload
 document.querySelector("#reload").addEventListener("click", () => {
 	log.textContent = "";
 	document.location.reload();
