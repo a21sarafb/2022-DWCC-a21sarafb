e6550b29dda777ccfd8c8e3cdaf551abce08faed
a21sarafb <a21sarafb@iessanclemente.net>
2023-01-26 10:11:14 +0100

    26-01-2023

--- exemplo1/2eval/Ajax/exercicios1.19Ajax.js
+++ exemplo1/2eval/Ajax/exercicios1.19Ajax.js
@@ -102,7 +102,7 @@ body.addEventListener("click", (event) => {
 				let newWin = window.open(
 					"about:blank",
 					"hello",
-					"width=500,height=300"
+					"width=510,height=300"
 				);
 				let texto =
 					"<strong>Capitalidad</strong> ->" +
