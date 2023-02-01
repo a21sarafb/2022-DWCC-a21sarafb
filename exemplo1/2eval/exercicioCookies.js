7e837dbee7d9ee462c6d18cf2c0f1c55342d7a74
a21sarafb <a21sarafb@iessanclemente.net>
2023-02-01 22:27:10 +0100

    01-02-2023

--- exemplo1/2eval/exercicioCookies.js
+++ exemplo1/2eval/exercicioCookies.js
@@ -8,29 +8,59 @@ valores introducidos. */
 
 //nome
 let nome = document.getElementById("nome");
-nome.addEventListener("blur", guardar);
+nome.addEventListener("blur", setItemFuncion);
 nome.value = localStorage.getItem("nome");
 nome.setAttribute("value", nome.value);
 
 //num
 let tel = document.getElementById("tel");
-tel.addEventListener("blur", guardar);
+tel.addEventListener("blur", setItemFuncion);
 tel.value = localStorage.getItem("tel");
 tel.setAttribute("value", tel.value);
 
-//select
+//select 1
 let select = document.getElementById("select");
-select.addEventListener("blur", guardar);
+console.log(select);
+select.addEventListener("blur", setItemFuncion);
 select.value = localStorage.getItem("select");
-//select.setAttribute("checked", true);
+select.setAttribute("checked", true);
+
+//select 2
+let select2 = document.getElementById("select2");
+console.log(select2);
+select2.addEventListener("blur", setItemFuncion);
+select2.value = localStorage.getItem("select2");
+select2.setAttribute("checked", true);
+
+//select 3
+let select3 = document.getElementById("select3");
+console.log(select3);
+select3.addEventListener("blur", setItemFuncion);
+select3.value = localStorage.getItem("select3");
+select3.setAttribute("checked", true);
 
 //check
 let checkbox = document.getElementById("check");
-checkbox.addEventListener("click", guardar);
+console.log(checkbox);
+checkbox.addEventListener("click", setItemFuncion);
 checkbox.checked = localStorage.getItem("check");
-//checkbox.setAttribute("checked", true);
+checkbox.setAttribute("checked", true);
 
-function guardar(e) {
+//check2
+let checkbox2 = document.getElementById("check2");
+console.log(checkbox);
+checkbox2.addEventListener("click", setItemFuncion);
+checkbox2.checked = localStorage.getItem("check2");
+checkbox2.setAttribute("checked", true);
+
+//check3
+let checkbox3 = document.getElementById("check3");
+console.log(checkbox);
+checkbox3.addEventListener("click", setItemFuncion);
+checkbox3.checked = localStorage.getItem("check3");
+checkbox3.setAttribute("checked", true);
+
+function setItemFuncion(e) {
 	console.log(e.target.id);
 	if (e.target.id == "nome") {
 		localStorage.setItem("nome", nome.value);
@@ -38,7 +68,15 @@ function guardar(e) {
 		localStorage.setItem("tel", tel.value);
 	} else if (e.target.id == "select") {
 		localStorage.setItem("select", select.value);
+	} else if (e.target.id == "select2") {
+		localStorage.setItem("select2", select2.value);
+	} else if (e.target.id == "select3") {
+		localStorage.setItem("select3", select3.value);
 	} else if (e.target.id == "check") {
 		localStorage.setItem("check", checkbox.checked);
+	} else if (e.target.id == "check2") {
+		localStorage.setItem("check2", checkbox2.checked);
+	} else if (e.target.id == "check3") {
+		localStorage.setItem("check3", checkbox3.checked);
 	}
 }
