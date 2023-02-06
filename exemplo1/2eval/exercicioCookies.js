3327c45e559d52bd058eda4860f74a15eb1352d7
a21sarafb <a21sarafb@iessanclemente.net>
2023-02-06 17:54:23 +0100

    06-02-2023

--- exemplo1/2eval/exercicioCookies.js
+++ exemplo1/2eval/exercicioCookies.js
@@ -14,20 +14,11 @@ function funcionCambios(e) {
 		localStorage.setItem("nome", nome.value);
 	} else if (e.target.id == "tel") {
 		localStorage.setItem("tel", tel.value);
-	} else if (e.target.id == "select") {
-		localStorage.setItem("select", select.value);
-		//console.log(localStorage.getItem("select"));
-		/*if (localStorage.getItem("select2")) {
-			select.setAttribute("checked", true);
-			localStorage.removeItem("select2");
-		}*/
+	} else if (e.target.id == "select1") {
+		localStorage.setItem("select1", select1.value);
 	} else if (e.target.id == "select2") {
+		console.log(select2.value);
 		localStorage.setItem("select2", select2.value);
-		//console.log(localStorage.getItem("select2"));
-		/*if (localStorage.getItem("select")) {
-			select2.setAttribute("checked", true);
-			localStorage.removeItem("select");
-		}*/
 	} else if (e.target.id == "check") {
 		localStorage.setItem("check", checkbox.checked);
 	} else if (e.target.id == "check2") {
@@ -45,12 +36,10 @@ let tel = document.getElementById("tel");
 tel.value = localStorage.getItem("tel");
 
 //select 1
-let select = document.getElementById("select");
-select.value = localStorage.getItem("select");
+let select1 = document.getElementById("select12");
 
 //select 2
 let select2 = document.getElementById("select2");
-select2.value = localStorage.getItem("select2");
 
 //check
 let checkbox = document.getElementById("check");
@@ -68,39 +57,45 @@ function recargaPaxina() {
 	for (let i = 0; i < localStorage.length; i++) {
 		//localStorage.removeItem(localStorage.key(i));
 		//console.log(localStorage.key(i));
-		if (localStorage.key(i) == "select") {
-			console.log("hola select");
-			console.log(localStorage.getItem("select"));
-			//select.setAttribute("checked", true);
+		if (localStorage.key(i) == "select1") {
+			console.log("hola select1");
+			console.log(localStorage.getItem("select1"));
+			if (localStorage.getItem("select2") === "completo") {
+				select1.setAttribute("checked", true);
+				localStorage.removeItem("select2");
+			}
 		} else if (localStorage.key(i) == "select2") {
 			console.log("hola select 2");
 			console.log(localStorage.getItem("select2"));
-			select2.setAttribute("checked", true);
+			if (localStorage.getItem("select2") === "medio") {
+				select2.setAttribute("checked", true);
+				localStorage.removeItem("select1");
+			}
 		} else if (localStorage.key(i) == "check") {
 			console.log(localStorage.getItem("check"));
-			if (localStorage.getItem("check")) {
+			if (localStorage.getItem("check") === "true") {
 				console.log("entro1");
 				checkbox.setAttribute("checked", true);
-			} else {
-				localStorage.removeItem(localStorage.key(i));
-				checkbox3.setAttribute("checked", false);
+			} else if (localStorage.getItem("check") === "false") {
+				console.log("entro else1");
+				localStorage.removeItem("check");
 			}
 		} else if (localStorage.key(i) == "check2") {
 			console.log(localStorage.getItem("check2"));
-			if (localStorage.getItem("check2")) {
+			if (localStorage.getItem("check2") === "true") {
 				console.log("entro2");
 				checkbox2.setAttribute("checked", true);
-			} else {
-				localStorage.removeItem(localStorage.key(i));
-				checkbox2.setAttribute("checked", false);
+			} else if (localStorage.getItem("check2") === "false") {
+				console.log("entro else2");
+				localStorage.removeItem("check2");
 			}
 		} else if (localStorage.key(i) == "check3") {
 			console.log(localStorage.getItem("check3"));
-			if (localStorage.getItem("check3")) {
+			if (localStorage.getItem("check3") === "true") {
 				console.log("entro3");
-				checkbox3.setAttribute("checked", true);
-			} else {
-				localStorage.removeItem(localStorage.key(i));
+			} else if (localStorage.getItem("check3") === "false") {
+				console.log("entro else3");
+				localStorage.removeItem("check3");
 			}
 		}
 	}
