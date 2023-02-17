d85930fe2b9454d2575d40608c0b8595208a062a
a21sarafb <a21sarafb@iessanclemente.net>
2023-02-17 10:24:13 +0100

    17-02-2023

--- exemplo1/2eval/Vue/ejercicios/ej7.js
+++ exemplo1/2eval/Vue/ejercicios/ej7.js
@@ -1,14 +1,37 @@
 Vue.createApp({
 	data() {
 		return {
-			styleColor: {},
+			isRed: false,
+			isRed2: false,
 		};
 	},
 	methods: {
-		cambio() {
-			this.styleColor = {
-				backgroundColor: "red",
-			};
+		cambio(e) {
+			if (
+				e.currentTarget.id == "1" &&
+				this.isRed2 == false &&
+				this.isRed == false
+			) {
+				this.isRed = true;
+				this.isRed2 = false;
+			} else if (e.currentTarget.id == "1" && this.isRed2) {
+				this.isRed2 = false;
+				this.isRed = true;
+			} else if (e.currentTarget.id == "1" && this.isRed) {
+				this.isRed = false;
+			} else if (
+				e.currentTarget.id == "2" &&
+				this.isRed == false &&
+				this.isRed2 == false
+			) {
+				this.isRed2 = true;
+				this.isRed = false;
+			} else if (e.currentTarget.id == "2" && this.isRed) {
+				this.isRed = false;
+				this.isRed2 = true;
+			} else if (e.currentTarget.id == "2" && this.isRed2) {
+				this.isRed2 = false;
+			}
 		},
 	},
 }).mount("#data");
