8fac96b3d14d3b31a10095ae6f582ad61fb5b8f3
a21sarafb <a21sarafb@iessanclemente.net>
2023-02-28 10:13:29 +0100

    28-02-2023

---
+++ exemplo1/2eval/Vue/ejercicios/proba1/src/components/HelloWorld.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+	// Escribir as opcións do compoñente: data, methods, watch, … en caso necesario
+	data() {},
+};
+</script>
+
+<template>
+	<h2>ola mundo</h2>
+</template>
+
+<style scoped>
+.read-the-docs {
+	color: #888;
+}
+</style>
